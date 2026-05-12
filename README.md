776255b83c482a8a27a8727306e4513272cc9428
dowellsHead <xkopylova@gmail.com>
2026-05-12 20:34:13 +0300

    add Grid creation and brush functionality

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # etch-a-sketch
-fun excercise 
- move your mouse across the grid
- try to draw a cat, maybe?
+A small excercise in JS I made while completing The Odin Project.
+Input the grid size (inputing n results in n x n grid, max is 100) and move your mouse across the grid. It should leave a trail like a pen would.
+Draw a cat, maybe?
--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 function createGrid(n){
-    console.log('beanz');
+    let divCount = n*n;
+    let divSize = 500/n;
     const main = document.querySelector('#container');
+    main.innerHTML = "";
     for (let i = 0; i<n; i++){
         for (let j = 0; j<n; j++){
             const newDiv = document.createElement('div');
             newDiv.classList.add('content');
-            newDiv.textContent = 'yay!';
+            newDiv.style.border = "1px solid black";
             newDiv.setAttribute('class', 'cell');
+            newDiv.style.width = divSize+'px';
+            newDiv.style.height = divSize+'px';
             main.appendChild(newDiv);
+            newDiv.addEventListener("mouseover", function (e) {
+                
+                e.target.style.background = "blue";
+            });
         }
     }
+    
+function randomColor(){
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
 
 }
 
-createGrid(16);
+}
+
+const btn = document.querySelector('#sizeSelector');
+btn.addEventListener("click", function (e) {
+    let size = prompt("Please input the grid size n \n(the grid size is n by n, the max n is 100)");
+    if (size<=100 && size>0) {
+        createGrid(size);
+    }
+    else
+        alert('Invalid size. Please, try again');
+}); 
+
+
